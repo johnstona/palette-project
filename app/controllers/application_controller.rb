04ac294e233d15f447761e3475cc721661e35533
Alex Johnston <alexjohnston@ymail.com>
2019-06-14 15:05:51 +0100

    "created logged_in? method"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
 
-    # helper_method: logged_in
+    helper_method: logged_in
 
-    # def logged_in
+    def logged_in?
+        !!session[:user_id]
+    end
 
-    # end
-
-    # def current_user
-
-    # end
+    
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,14 @@ class LikesController < ApplicationController
     def new
         @like = Like.new
     end
+
+    def create
+        if logged_in?
+            like = Like.create like_params
+            redirect_to like
+        else
+            redirect_to :login
+        end
+    end
+    
 end
