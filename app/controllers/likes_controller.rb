5514da814d01100d4ace0edc401050fb5dc57ca5
Alex Johnston <alexjohnston@ymail.com>
2019-06-14 15:22:29 +0100

    likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,6 +9,7 @@ class LikesController < ApplicationController
             like = Like.create like_params
             redirect_to like
         else
+            flash[:errors] = like.errors.full_messages
             redirect_to :login
         end
     end
