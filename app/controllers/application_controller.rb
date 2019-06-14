e2e26741d6144113ce93fe8cc7edce5c8e92c4b7
Alex Johnston <alexjohnston@ymail.com>
2019-06-14 15:14:54 +0100

    cleaner ApplicationController methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
 
-    helper_method: logged_in
+    helper_method :current_user, :logged_in?
 
+    def current_user
+        @current_user ||= User.find_by_id(session[:user])
+      end
+  
     def logged_in?
-        !!session[:user_id]
+        current_user != nil
     end
 
-    
 end
