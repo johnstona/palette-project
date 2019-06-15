2b904c5b212d771f485327216a203d72405ccaa1
Alex Johnston <alexjohnston@ymail.com>
2019-06-15 17:27:05 +0100

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :likes
     has_many :comments
     has_many :palettes
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    # has_secure_password
+
 end
