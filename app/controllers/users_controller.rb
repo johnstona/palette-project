081d03afdb26451b5edda2ea83ab8483d22b5216
Alex Johnston <alexjohnston@ymail.com>
2019-06-15 17:24:36 +0100

    users controller new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            redirect_to palettes_path
+        else
+            flash[:errors] = user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
