126995d536bed4240dd6da5450180fa264b9c4bc
Alex Johnston <alexjohnston@ymail.com>
2019-06-15 17:19:15 +0100

    created sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
+ 
   def new
+    @user = User.new
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if !user || !user.authenticate(params[:password])
+      flash[:errors] = ["Credentials aren't valid"]
+      redirect_to new_session_path
+    else
+      session[:user_id] = user.id
+      redirect_to new_answer_path
+    end
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
+  
 end
