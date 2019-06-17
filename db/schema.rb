441b17a04ca996114725ddcb6753528185ca064b
Alex Johnston <alexjohnston@ymail.com>
2019-06-17 09:24:11 +0100

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_135313) do
+ActiveRecord::Schema.define(version: 2019_06_17_081739) do
 
   create_table "colours", force: :cascade do |t|
     t.integer "palette_id"
@@ -21,33 +21,34 @@ ActiveRecord::Schema.define(version: 2019_06_13_135313) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "palette_id"
+    t.integer "pallette_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["palette_id"], name: "index_comments_on_palette_id"
+    t.index ["pallette_id"], name: "index_comments_on_pallette_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "palette_id"
+    t.integer "pallette_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["palette_id"], name: "index_likes_on_palette_id"
+    t.index ["pallette_id"], name: "index_likes_on_pallette_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "palettes", force: :cascade do |t|
+  create_table "pallettes", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_palettes_on_user_id"
+    t.index ["user_id"], name: "index_pallettes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
