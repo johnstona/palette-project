1f37854bc0f6069d8acba00dbb7d12188cc46717
Alex Johnston <alexjohnston@ymail.com>
2019-06-17 14:21:37 +0100

    updated like validations

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -3,4 +3,9 @@ class Palette < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :colours
+
+  def self.top_three_liked
+    self.all.sort_by{|palette| palette.likes.count}.reverse.limit(3)
+  end
+
 end
