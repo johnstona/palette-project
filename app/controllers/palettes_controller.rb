2d9855c21a9d0caf8a1fd1c8bbd3787f9210ec5e
moniet <moniet.sawhney@gmail.com>
2019-06-18 18:25:34 +0100

    added new stylesheet for landing page, improved ui, and made the page responsive

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -6,10 +6,10 @@ class PalettesController < ApplicationController
 
     def new
       @palette = Palette.new
-      # nums = params[:num].to_i
-      # nums.times do |num|
-      #   @palette.colours.build(hex_code: "#fff")
-      # end
+      nums = params[:num].to_i
+      nums.times do |num|
+        @palette.colours.build(hex_code: "#fff")
+      end
     end
 
     def create
