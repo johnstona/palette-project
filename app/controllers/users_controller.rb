a22e93a7c5419dcdcf8ce44b17cdc8c3778a91b1
Alex Johnston <alexjohnston@ymail.com>
2019-06-19 11:03:30 +0100

    chaged new user redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            redirect_to user_path(user)
+            redirect_to index_path
         else
             flash[:errors] = user.errors.full_messages
             redirect_to new_user_path
