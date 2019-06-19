184d463bbf2c9d452f30fb00f018c51368912e8d
tstiddy <spaceinvadini99@gmail.com>
2019-06-19 10:26:30 +0100

    fixed comments and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     def show
       user = User.find(params[:id])
       @username = user.username
+      @user_palette = user
     end
 
     private
