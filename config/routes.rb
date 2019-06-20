025fc16445524fe2a7e4f9f01df37b6acd8c15a9
Alex Johnston <alexjohnston@ymail.com>
2019-06-20 15:59:39 +0100

    example routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/', to: 'palettes#index', as: "index"
 
   get 'palettes/new/:num', to: 'palettes#new', as: 'new_palette'
+  get 'palettes/:id/example', to: 'palettes#example', as: 'example_palette'
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
